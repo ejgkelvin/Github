9a04fd53b59279fd2e75a90dc3e71a71197de5b1
Chen Chu <chen_chu@dfci.harvard.edu>
2020-03-24 15:45:08 -0400

    Feasible batch_Cufflinks

--- batch_Cufflinks.sh
+++ batch_Cufflinks.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
-#initialize a variable with an intuitive name to store the name of the input fastq file
-fq=$1
+bm=$1
 
 #grab base of file name for naming outputs
-base=`basename $fq .fastqAligned.sortedByCoord.out.bam`
+base=`basename $bm Aligned.sortedByCoord.out.bam`
 echo "Sample name is $base"
+
 
 #load modules needed for this script
 module load cufflinks/2.2.1
 
 #run the script
-cufflinks $fq \
- --GTF /home/cc452/sicinskilab/reference_data/Mus_musculus.GRCm38.92.gtf \
- -p 8 \
- --upper-quartile-norm \
- --multi-read-correct \
- -o ./$base
-     
+ cufflinks -o ${base}_cufflinks -G /home/cc452/sicinskilab/reference_data/Mus_musculus.GRCm38.92.gtf --num-threads 20 --upper-quartile-norm --total-hits-norm $1 
